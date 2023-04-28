57b0ec44fada88c080d774941ca918d49581ebdd
JoseMCL2 <131408706+JoseMCL2@users.noreply.github.com>
2023-04-28 19:39:14 -0400

    calculo promedio de 4 notas

---
+++ calcular promedio de 4 notas.py
@@ -0,0 +1,6 @@
+nota1=int(input("ingrese la primera nota:"))
+nota2=int(input("ingrese la segunda nota:"))
+nota3=int(input("ingrese la tercera nota:"))
+nota4=int(input("ingrese la cuarta nota:"))
+promedio=(nota1+nota2+nota3+nota4)/4
+print("el promedio de las 4 notas es:",promedio)
